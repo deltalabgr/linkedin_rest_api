b1b184ea5b8d4d4bff560b9dc99330ce5da25d3f
Dimitrios Douros <dourosdimitris@outlook.com>
2023-11-23 15:38:16 +0200

    init

---
+++ lib/linkedin_rest_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "linkedin_rest_api/version"
+
+module LinkedinRestApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/linkedin_rest_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LinkedinRestApi
+  VERSION = "0.1.0"
+end
